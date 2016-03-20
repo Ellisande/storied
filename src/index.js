9b3f6b95d32ec1e5a334eacf147e7a288ca5cf3e
Ellisande <justin.dragos@gmail.com>
2016-03-19 17:59:07 -0700

    Added an initial implementation for parsing a file.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-console.log('Hello world');
+import Story from './models/story';
+import StepParser from './parsers/stepParser';
+import ScenarioParser from './parsers/scenarioParser';
+import {MetaDataParser, MetaParser} from './parsers/metaParser';
+
+const parsers = [StepParser, ScenarioParser, MetaParser, MetaDataParser];
+
+var fs = require('fs');
+fs.readFile(process.cwd() + '/test/stories/test.story', function(err, data) {
+  if (err) {
+    throw err;
+  }
+  var array = data.toString().split('\n');
+  var myStory;
+  var lastSection;
+  array.forEach(line => {
+    if(!line || line.length === 0){
+      return;
+    }
+    const parser = parsers.find(currentParser => currentParser.matches(line));
+    if(!parser){
+      throw new Error(`Could not find a parser for this line. Check your syntax and try again. Line: ${line}`);
+    }
+    const parsedLine = new parser(line, lastSection);
+    if(parsedLine.scenario){
+      myStory = new Story(parsedLine.scenario);
+    }
+    if(parsedLine.meta){
+      myStory.addMetaData(parsedLine.meta);
+    }
+    if(parsedLine.step){
+      myStory.addStep(parsedLine.step);
+    }
+    lastSection = parsedLine.section;
+  });
+  console.log(myStory.toString());
+});
